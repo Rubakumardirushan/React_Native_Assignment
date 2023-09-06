ff2e3c8b274cbe355521bcd1023f12f9f801995a
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-09-06 10:46:53 +0530

    Login Screen Added

---
+++ Mobile_React_Native/components/Login.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet ,TextInput} from 'react-native';
+import { WingBlank,Button } from '@ant-design/react-native';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { login } from '../Actions/authAction';
+
+const Login = ({ navigation }) =>{
+  
+    const [before,after]=useState('')
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState(''); 
+    const dispatch = useDispatch();
+    const handleLogin = () => {
+      const credentials = { username, password ,navigation};
+      dispatch(login(credentials))
+   
+    };
+
+
+
+  return (
+    
+    <View style={styles.container}>
+      <WingBlank>
+
+        <Text style={styles.title}>LOGIN PAGE</Text>
+
+        <TextInput style={styles.inputbox}
+        value={username}
+        placeholder='  USERNAME'  onChangeText={(text) => setUsername(text)}></TextInput>
+
+        <TextInput
+        placeholder='  PASSWORD'onChangeText={(text) => setPassword(text)}  style={styles.inputbox}
+        secureTextEntry
+        ></TextInput>
+
+        <Button style={styles.buttons} onPress={handleLogin} >LOG IN</Button>
+
+        <Text style={styles.results}
+        >{before}</Text>
+
+      </WingBlank>
+    </View>
+   
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    marginBottom:40,
+    marginRight:20,
+    marginLeft:20,
+    marginTop:40,
+    borderRadius: 30, 
+    borderWidth: 1, 
+    
+    
+    
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+   marginLeft:32,
+   marginTop:155,
+   fontFamily:'Raleway'
+   
+
+  },
+  inputbox:{
+    marginTop:30,
+    marginLeft:20,
+    marginRight:20,
+    borderRadius: 35, 
+    borderWidth: 2, 
+    borderColor: 'white',
+    textAlign:'auto',
+    fontSize:20,
+    padding:15
+    
+  },
+  buttons:{
+    backgroundColor:'white',
+marginTop:25,
+marginRight:70,
+marginLeft:70,
+borderRadius: 30,
+borderColor: 'black',
+borderWidth: 2,
+  },
+  results:{
+marginTop:90,
+marginLeft:30,
+fontSize:20,
+fontWeight:'bold',
+
+  }
+});
+
+export default Login;

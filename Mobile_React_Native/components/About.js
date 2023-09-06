8b7e4d8a2d42f6aecd6e15ae219a336756a86b46
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-09-06 13:20:59 +0530

    Navigation Added

---
+++ Mobile_React_Native/components/About.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, } from 'react-native';
+import { Button } from '@ant-design/react-native';
+import axios from 'axios';
+import { Card, ListItem } from 'react-native-elements';
+
+
+
+const About = ({navigation}) => {
+    const [products, setProducts] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get('https://fakestoreapi.com/products')
+        .then((response) => {
+          setProducts(response.data);
+          
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+  const sortpage=()=>{
+    navigation.navigate('sortlist')
+  }
+    const renderItem = ({ item }) => (
+      <View style={{ marginTop:20 ,padding:10,backgroundColor: '#BECEE7',}}>
+      <Card>
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Divider />
+        <Card.Image source={{ uri: item.image }} />
+        <Text style={{ marginVertical: 10 }}>{item.description}</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text>Price: ${item.price}</Text>
+          <Text>Category: {item.category}</Text>
+         
+        </View>
+      </Card>
+      </View>
+    );
+  
+    return (
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        ListFooterComponent={
+            <View >
+              {<Button  style={{ borderRadius:20,width:160,backgroundColor:'white' ,marginLeft:100,marginTop:10}}  onPress={sortpage} >Next</Button>}
+              
+            </View>
+          }
+      />
+    );
+  };
+  
+  export default About;
+  
---
+++ Mobile_React_Native/components/AppNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import store from '../store';
+
+import Login from './Login';
+import About from './About';
+import Sort from './Sort ';
+import Search from './Search';
+import Limit from './Limit';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => (
+  <Provider store={store}>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="LOGIN PAGE">
+      <Stack.Screen name="LOGIN PAGE" component={Login} />
+      <Stack.Screen name="LIST ALL AVAILABLE PRODUCTS" component={About} />
+      <Stack.Screen name="sortlist" component={Sort}/>
+      <Stack.Screen name="SearchScreen" component={Search}/>
+      <Stack.Screen name="LimitScreen" component={Limit}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+  </Provider>
+);
+
+export default AppNavigator;

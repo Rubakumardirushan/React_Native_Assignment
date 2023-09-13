8417b72e9047a938d0575393c76a44af7b8882c6
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-09-13 11:35:37 +0530

    Update Login.js

--- Mobile_React_Native/components/Login.js
+++ Mobile_React_Native/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { View, Text, StyleSheet ,TextInput} from 'react-native';
 import { WingBlank,Button } from '@ant-design/react-native';
 

67d6a59bb89df4dd78105f5427ce14c5232bc713
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-09-15 07:56:22 +0530

    Update Search.js

--- Mobile_React_Native/components/Search.js
+++ Mobile_React_Native/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { View, Text, TextInput,  FlatList ,Alert,StyleSheet} from 'react-native';
 import axios from 'axios';
 import { Button } from '@ant-design/react-native';
